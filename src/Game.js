5223902408a94435eec383d0ef3c87b289f7dd59
xxBogdan <poprik.bogdan2014@yandex.ru>
2021-02-14 17:51:20 +0400

    game

--- src/Game.js
+++ src/Game.js
@@ -3,116 +3,141 @@ import './game.css';
 
 class Game extends Component {
 
-    constructor(props) 
+	constructor(props) 
     {
-        super(props);
+		super(props);
         this.state = {
-          min: 1,
-          max: 10,
-          number: 1
+            min: 1,
+            max: 10,
+            number: 1
+          }
+        this.timerActive="";
+		this.numberAttempts = 3;
+        this.boxClass = [
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "",
+          "", 
+          "",
+          "",
+          "",
+          "",
+          "", 
+          "",
+          "",
+          "",
+          "",
+          "", 
+          "",
+          "",
+          "",
+          "",
+          "", 
+        ];
+		
+        this.state = {
+            boxClass: this.boxClass
         }
-    }
+          
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    componentDidMount() 
+    handleClick = (e) => 
     {
-        this.setState({ number: this.generateNumber(this.state.min, this.state.max)})
-    }
-       
-    generateNumber = (min, max) => 
-    {
-        return Math.floor(Math.random()*(max-min+1)+min)
-    }
-       
-    getInputs = () => 
-    {
-        if(this.state.min > this.state.max )
-        {
-        
-          const minTemp = this.state.min
-          const maxTemp = this.state.max
-
-          this.setState
-          (
-            { 
-                min: maxTemp,
-                max: minTemp
-            }, () =>
-            this.setState
-            ({
-              number: this.generateNumber(this.state.min, this.state.max)  
-            })
-          );
-        } 
-        
-        else 
-        {
-          this.setState
-          ({
-            number: this.generateNumber(this.state.min, this.state.max)  
-          })
-        }
-
         const min = 1;
         const max = 10;
-        const rand = Math.floor(Math.random()*(max-min+1)+min);
+        const rand = Math.floor(Math.random() * (max - min + 1) + min);
+        let delayWait = 5000;
+        let attempts = document.getElementById("attempts");
+        let text = document.getElementById("notification_text");
+        let keyId = e.currentTarget.dataset.id;
+        text.innerHTML = "Ячейка откроется через " + delayWait/1000 + " секунд";
+        console.log(e.target.dataset.id);
 
-        if (rand > 5)
+        if (this.numberAttempts === 0)
         {
-            document.getElementById("notification_text").innerHTML="Вы выиграли!";
+            text.innerHTML = "Ваши попытки закончились!";
+            return false;
         }
 
-        else
+        else 
         {
-            document.getElementById("notification_text").innerHTML="Вы проиграли!";
-        }
-    }
+            setTimeout(() => 
+            {
+                if (rand >= 4 && rand < 8)
+                {
+                    text.innerHTML = "Вы выиграли!";
+                    this.boxClass[keyId]="win";
+                }
 
-    /*handleClick = (e) => 
-    {
-        //e.target.innerHTML = "";
-        document.getElementById("notification_text").innerHTML="Кнопка нажата " + e.target.innerHTML;
-    }*/
+                else if (rand >= 8 && rand < 10)
+                {
+                    text.innerHTML = "Вы получили дополнительную попытку!";
+                    this.boxClass[keyId]="draw"
+                }
+
+                else
+                {
+                    text.innerHTML = "Вы проиграли.";
+                    this.boxClass[keyId]="lose"
+                }
+            
+                attempts.innerHTML = "Попытки: " + this.numberAttempts;
+                this.setState({boxClass:this.boxClass})
+
+            }, delayWait);
+        }
+    } 
 
     render() 
     {
         return (
             <div className="games">
-                <div className="notification"><p id="notification_text"></p></div>
+                <div className="notification">
+                    <p id="notification_text">Выберите ячейку для запуска игры!</p>
+                    <p id="attempts">Попытки: {this.numberAttempts}</p>
+                </div>
                     <div className="wrapper_space">
                         <div className="line_one">
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>{ this.state.number }</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>A2</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>A3</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>A4</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>A5</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="0" className={this.boxClass[0]}><h3>A1</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="1" className={this.boxClass[1]}><h3>A2</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="2" className={this.boxClass[2]}><h3>A3</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="3" className={this.boxClass[3]}><h3>A4</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="4" className={this.boxClass[4]}><h3>A5</h3></button></div>
                         </div>
                         <div className="line_two">
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>B1</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>B2</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>B3</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>B4</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>B5</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="5" className={this.boxClass[5]}><h3>B1</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="6" className={this.boxClass[6]}><h3>B2</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="7" className={this.boxClass[7]}><h3>B3</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="8" className={this.boxClass[8]}><h3>B4</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="9" className={this.boxClass[9]}><h3>B5</h3></button></div>
                         </div>
                         <div className="line_three">
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>C1</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>C2</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>C3</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>C4</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>C5</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="10" className={this.boxClass[10]}><h3>C1</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="11" className={this.boxClass[11]}><h3>C2</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="12" className={this.boxClass[12]}><h3>C3</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="13" className={this.boxClass[13]}><h3>C4</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="14" className={this.boxClass[14]}><h3>C5</h3></button></div>
                         </div>
                         <div className="line_four">
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>D1</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>D2</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>D3</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>D4</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>D5</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="15" className={this.boxClass[15]}><h3>D1</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="16" className={this.boxClass[16]}><h3>D2</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="17" className={this.boxClass[17]}><h3>D3</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="18" className={this.boxClass[18]}><h3>D4</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="19" className={this.boxClass[19]}><h3>D5</h3></button></div>
                         </div>
                         <div className="line_five">
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>F1</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>F2</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>F3</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>F4</h3></button></div>
-                            <div className="cell"><button onClick={this.handleClick} onClick={ this.getInputs }><h3>F5</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="20" className={this.boxClass[20]}><h3>F1</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="21" className={this.boxClass[21]}><h3>F2</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="22" className={this.boxClass[22]}><h3>F3</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="23" className={this.boxClass[23]}><h3>F4</h3></button></div>
+                            <div className="cell"><button onClick={this.handleClick} data-id="24" className={this.boxClass[24]}><h3>F5</h3></button></div>
                         </div>
                     </div>
             </div>
